94c33160f2c3edeef046e854467fc565db6bc7f5
IlyaOsotov <soten58@yandex.ru>
2018-10-30 23:14:18 +0300

    removed config file

--- src/apiConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAP2OoS3J3NVwiDeFfc9n8k8zlahbKTj68",
-  authDomain: "vue-reddit-place.firebaseapp.com",
-  databaseURL: "https://vue-reddit-place.firebaseio.com",
-  projectId: "vue-reddit-place",
-  storageBucket: "vue-reddit-place.appspot.com",
-  messagingSenderId: "806415192147"
-};
-// TODO: add to credentials 
