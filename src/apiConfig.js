890ab88b0a39625823bab5c310c49debcd7aebf2
IlyaOsotov <soten58@yandex.ru>
2018-10-30 23:01:49 +0300

    added dockerfile

---
+++ src/apiConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAP2OoS3J3NVwiDeFfc9n8k8zlahbKTj68",
+  authDomain: "vue-reddit-place.firebaseapp.com",
+  databaseURL: "https://vue-reddit-place.firebaseio.com",
+  projectId: "vue-reddit-place",
+  storageBucket: "vue-reddit-place.appspot.com",
+  messagingSenderId: "806415192147"
+};
+// TODO: add to credentials 
